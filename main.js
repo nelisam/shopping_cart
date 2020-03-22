2af9c06c0be400cd39026e59156857cd6e1cb6ba
nelisam <nelisamaqungo@gmail.com>
2020-03-23 00:51:34 +0200

    Cart added , next up add functionality to remove, increase and decrease icons

--- main.js
+++ main.js
@@ -103,4 +103,46 @@ function totalCost(product) {
     
 }
 
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart")
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+
+    if (cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle-outline"></ion-icon>
+                <img src="../images/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            <div class="price">R ${item.price},00</div>
+            <div class="quantity">
+                <ion-icon class="decrease" name="arrow-back-circle"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon class="increase" name="arrow-forward-circle"></ion-icon>
+            </div>
+            <div class="total">
+                R ${item.inCart * item.price},00
+            </div>
+            `
+        });
+
+        productContainer.innerHTML += `
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">
+                    Basket Total
+                </h4>
+                <h4 class="basketTotal">
+                    R ${cartCost},00
+                </h4>
+            </div>
+        
+        `;
+    }
+}
+
 onLoadCartNumbers();
+displayCart();
